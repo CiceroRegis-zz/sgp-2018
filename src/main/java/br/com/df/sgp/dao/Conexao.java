321b3db85e8b0ce83c64b92682b82d3f0a39823b
CiceroRegis <ciceroregis25@gmail.com>
2018-09-12 17:23:44 -0300

    commit

--- src/main/java/br/com/df/sgp/dao/Conexao.java
+++ src/main/java/br/com/df/sgp/dao/Conexao.java
@@ -38,13 +38,13 @@ public class Conexao {
 	}*/
 	
 /*
- * Heroku
- * 
+ * Heroku 
+ * @amazonaws
  */
 	private static String driver = "org.postgresql.Driver";
 	private static String usuario = "yvutrjblyrymne";
 	private static String senha = "e7b24e3671833f375ffb5241c84ea3153849edc494149ae9f264fc896885a380";
-	private static String url = "jdbc:postgresql://eec2-54-227-241-179.compute-1.amazonaws.com:5432/dbfe59pn81s4bl?sslmode=require";
+	private static String url = "jdbc:postgresql://ec2-54-227-241-179.compute-1.amazonaws.com:5432/dbfe59pn81s4bl?sslmode=require";
 	
 	public static  Connection getConnection() throws SQLException{
 		//Context ctx = null;
