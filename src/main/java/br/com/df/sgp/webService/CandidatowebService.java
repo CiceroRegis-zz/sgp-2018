f1835b39ba0dfb019500b880df7e5aa4df0e1d27
CiceroRegis <ciceroregis25@gmail.com>
2018-09-18 18:55:40 -0300

    nova classe

--- src/main/java/br/com/df/sgp/webService/CandidatowebService.java
+++ src/main/java/br/com/df/sgp/webService/CandidatowebService.java
@@ -1,7 +1,11 @@
 package br.com.df.sgp.webService;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
@@ -20,75 +24,110 @@ public class CandidatowebService {
 
 	Candidato candidato = new Candidato();
 
+	List<Candidato> candidatos = new ArrayList<>();
 	
-	public List<Candidato> listaCandidatosGovernador(){
-		List<Candidato> candidatosGovernador = new ArrayList<>();
+	
+	public Map<String, List<String>> listaCandidatosGovernador(){
+		Map<String, List<String>> nomes = new TreeMap<>();
 		
-		candidatosGovernador = new ArrayList<>();
-		
-		candidatosGovernador.add(new Candidato("ALBERTO FRAGA	","DEM	"));
-		candidatosGovernador.add(new Candidato("IBANEIS","DEM	"));
-		candidatosGovernador.add(new Candidato("ALEXANDRE GUERRA	","NOVO	"));
-		candidatosGovernador.add(new Candidato("RENAN ROSA ","PCO"));
-		candidatosGovernador.add(new Candidato("ELIANA PEDROSA ","PROS"));
-		candidatosGovernador.add(new Candidato("GENERAL PAULO CHAGAS ","PRP"));
-		candidatosGovernador.add(new Candidato("RODRIGO ROLLEMBERG","	PSB"));
-		candidatosGovernador.add(new Candidato("ROGÉRIO ROSSO","PSD"));
-		candidatosGovernador.add(new Candidato("FÁTIMA SOUSA","PSOL"));
-		candidatosGovernador.add(new Candidato("GUILLEN","PSTU"));
-		candidatosGovernador.add(new Candidato("JÚLIO MIRAGAYA","PT"));
-		return candidatosGovernador;
+		String path = "E:\\Dev\\Projetos\\sgp-2018\\src\\main\\webapp\\pages\\csv\\candidatos_distrital.txt";
+		String str = "";
+		try (BufferedReader read  =  new BufferedReader(new FileReader(path))){
+			while((str = read.readLine()) !=null) {
+				String[] list = str.split("[;]");
+				String filds  = list[0].toString();
+				List<String>  governadores = nomes.get(filds);
+				if(governadores == null) {
+					governadores = new ArrayList<>();
+				}
+				
+				governadores.add(filds);
+				nomes.put(filds, governadores);
+			}
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+		return nomes;
 	}
 	
 	
-	public List<Candidato> listaCandidatosDristrital(){
-		List<Candidato> candidatosDistrital = new ArrayList<>();
-		
-		candidatosDistrital.add(new Candidato("ADRIANA GLORIA	","AVANTE"));
-		candidatosDistrital.add(new Candidato("AKILLA MARINHO	","AVANTE"));
-		candidatosDistrital.add(new Candidato("ALFREDO JUNIOR	","AVANTE"));
-		candidatosDistrital.add(new Candidato("ANGELA TEIXEIRA	","AVANTE"));
-		candidatosDistrital.add(new Candidato("ARTHUR CARDOSO	","AVANTE"));
-		candidatosDistrital.add(new Candidato("BILL SAÚDE	    ","AVANTE"));
-		candidatosDistrital.add(new Candidato("BISMARCK UBER, CABIFY E 99	","AVANTE"));
-		candidatosDistrital.add(new Candidato("BOLIVAR ROCHA	","AVANTE"));
-		candidatosDistrital.add(new Candidato("CAMARGO","AVANTE"));
-		candidatosDistrital.add(new Candidato("CAROL DOS ANIMAIS	","AVANTE"));
-		candidatosDistrital.add(new Candidato("CORREA LEÃO DE JUDÁ	","AVANTE"));
-		candidatosDistrital.add(new Candidato("CRISTIA LIMA	","AVANTE"));
-		candidatosDistrital.add(new Candidato("DELEGADO MAURO CEZAR	","AVANTE"));
-		candidatosDistrital.add(new Candidato("DRA. JANE	","AVANTE"));
-		candidatosDistrital.add(new Candidato("DR. CARLOS	","AVANTE"));
-		candidatosDistrital.add(new Candidato("DR. ROBERTO LUCENA	","AVANTE"));
-		candidatosDistrital.add(new Candidato("FLÁVIO CARDOSO	","AVANTE"));
-		candidatosDistrital.add(new Candidato("GABI DA JUVENTUDE","AVANTE"));
-		candidatosDistrital.add(new Candidato("GEORGIA QUARTIN	","AVANTE"));
-		candidatosDistrital.add(new Candidato("GORDINHO DA FARMÁCIA	","AVANTE"));
-		candidatosDistrital.add(new Candidato("ISAURA BARBOSA	","AVANTE"));
-		candidatosDistrital.add(new Candidato("JOAO CARDOSO PROFESSOR-AUDITOR	","AVANTE"));
-		candidatosDistrital.add(new Candidato("KLEYSON MORENO	","AVANTE"));
-		candidatosDistrital.add(new Candidato("KYARA ZARUTY LGBTI	","AVANTE"));
-		candidatosDistrital.add(new Candidato("LULO SENA	","AVANTE"));
-		candidatosDistrital.add(new Candidato("MAICON MIRANDA	","AVANTE"));
-		candidatosDistrital.add(new Candidato("MAJOR LOURIVAL	","AVANTE"));
-		candidatosDistrital.add(new Candidato("MARCOS MAURÍCIO	","AVANTE"));
-		candidatosDistrital.add(new Candidato("MC BANDIDA	","AVANTE"));
-		candidatosDistrital.add(new Candidato("MILENE DA CEILÂDIA	","AVANTE"));
-		candidatosDistrital.add(new Candidato("NERY DO BRASIL	","AVANTE"));
-		candidatosDistrital.add(new Candidato("PAULA ALVES PSICOPEDAGOGA	","AVANTE"));
-		candidatosDistrital.add(new Candidato("PR. CÉLIO SANTANA	","AVANTE"));
-		candidatosDistrital.add(new Candidato("PROF. DIEGO MATOS DA FERCAL	","AVANTE"));
-		candidatosDistrital.add(new Candidato("PROFESSOR NIVALDO	","AVANTE"));
-		candidatosDistrital.add(new Candidato("PROF. IRAN LIMA	","AVANTE"));
-		candidatosDistrital.add(new Candidato("PROF. SEVERINO CAJAZEIRAS	","AVANTE"));
-		candidatosDistrital.add(new Candidato("RAFAEL MOREIRA	","AVANTE"));
-		candidatosDistrital.add(new Candidato("REGINALDO SARDINHA	","AVANTE"));
-		candidatosDistrital.add(new Candidato("","AVANTE"));
-		
-		
-		return candidatosDistrital;
-		
+	
+	/*public Map<String, List<String>> listaCandidatosDistrital() {
+
+		Map<String, List<String>> nomes = new TreeMap<>();
+
+		String path = "E:\\Dev\\Projetos\\sgp-2018\\src\\main\\webapp\\pages\\csv\\Lista-Candidatos-Deputado Distrital-DISTRITO FEDERAL-2018.xls";
+		String str = "";
+
+		try (BufferedReader read = new BufferedReader(new FileReader(path));) {
+			while ((str = read.readLine()) != null) {
+				String[] list = str.split("[;]");
+				String filds = list[0].toString();
+				List<String> distritais = nomes.get(filds);
+				if (distritais == null) {
+					distritais = new ArrayList<>();
+				}
+				distritais.add(filds);
+				nomes.put(filds, distritais);
+
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return nomes;
+
 	}
+
+	*/
+	/*public Map<String, List<String>> listaCandidatosFederal(){
+		Map<String, List<String>> nomes = new TreeMap<>();
+		
+		String path = "E:\\Dev\\Projetos\\sgp-2018\\src\\main\\webapp\\pages\\csv\\Lista-Candidatos-Deputado Federal-DISTRITO FEDERAL-2018.xls";
+		String str = "";
+		
+		try (BufferedReader read  =  new BufferedReader(new FileReader(path))){
+			while((str = read.readLine()) !=null) {
+				String[] list = str.split("[;]");
+				String filds  = list[0].toString();
+				List<String>  federais = nomes.get(filds);
+				if(federais == null) {
+					federais = new ArrayList<>();
+				}
+				
+				federais.add(filds);
+				nomes.put(filds, federais);
+			}
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+		return nomes;
+	}
+	
+	
+	public Map<String, List<String>> listaCandidatosSenador(){
+		Map<String, List<String>> nomes = new TreeMap<>();
+		
+		String path = "E:\\Dev\\Projetos\\sgp-2018\\src\\main\\webapp\\pages\\csv\\Lista-Candidatos-Senador-DISTRITO FEDERAL-2018.xls";
+		String str = "";
+		
+		try (BufferedReader read  =  new BufferedReader(new FileReader(path))){
+			while((str = read.readLine()) !=null) {
+				String[] list = str.split("[;]");
+				String filds  = list[0].toString();
+				List<String>  senadores = nomes.get(filds);
+				if(senadores == null) {
+					senadores = new ArrayList<>();
+				}
+				
+				senadores.add(filds);
+				nomes.put(filds, senadores);
+			}
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+		return nomes;
+	}*/
 	
 	public Candidato getCandidato() {
 		return candidato;
@@ -98,5 +137,12 @@ public class CandidatowebService {
 		this.candidato = candidato;
 	}
 
+	public List<Candidato> getCandidatos() {
+		return candidatos;
+	}
+
+	public void setCandidatos(List<Candidato> candidatos) {
+		this.candidatos = candidatos;
+	}
 
 }
