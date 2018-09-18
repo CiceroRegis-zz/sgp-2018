caad9259b7a1b9e905737e06ba54c922de7bd90e
CiceroRegis <ciceroregis25@gmail.com>
2018-09-18 17:29:59 -0300

    nova classe

--- src/main/java/br/com/df/sgp/webService/CandidatowebService.java
+++ src/main/java/br/com/df/sgp/webService/CandidatowebService.java
@@ -1,11 +1,7 @@
 package br.com.df.sgp.webService;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
@@ -26,108 +22,23 @@ public class CandidatowebService {
 
 	List<Candidato> candidatos = new ArrayList<>();
 	
-	
-	public Map<String, List<String>> listaCandidatosGovernador(){
-		Map<String, List<String>> nomes = new TreeMap<>();
+	public List<Candidato> listaCandidatosGovernador(){
 		
-		String path = "E:\\Dev\\Projetos\\sgp-2018\\src\\main\\webapp\\pages\\csv\\Lista-Candidatos-Governador.xls";
-		String str = "";
-		try (BufferedReader read  =  new BufferedReader(new FileReader(path))){
-			while((str = read.readLine()) !=null) {
-				String[] list = str.split("[;]");
-				String filds  = list[0].toString();
-				List<String>  governadores = nomes.get(filds);
-				if(governadores == null) {
-					governadores = new ArrayList<>();
-				}
-				
-				governadores.add(filds);
-				nomes.put(filds, governadores);
-			}
-		}catch (Exception e) {
-			// TODO: handle exception
-		}
-		return nomes;
+		candidatos = new ArrayList<>();
+		
+		candidatos.add(new Candidato("ALBERTO FRAGA	","DEM	"));
+		candidatos.add(new Candidato("IBANEIS","DEM	"));
+		candidatos.add(new Candidato("ALEXANDRE GUERRA	","NOVO	"));
+		candidatos.add(new Candidato("RENAN ROSA ","PCO"));
+		candidatos.add(new Candidato("ELIANA PEDROSA ","PROS"));
+		candidatos.add(new Candidato("GENERAL PAULO CHAGAS ","PRP"));
+		candidatos.add(new Candidato("RODRIGO ROLLEMBERG","	PSB"));
+		candidatos.add(new Candidato("ROGÉRIO ROSSO","PSD"));
+		candidatos.add(new Candidato("FÁTIMA SOUSA","PSOL"));
+		candidatos.add(new Candidato("GUILLEN","PSTU"));
+		candidatos.add(new Candidato("JÚLIO MIRAGAYA","PT"));
+		return candidatos;
 	}
-	
-	
-	
-	/*public Map<String, List<String>> listaCandidatosDistrital() {
-
-		Map<String, List<String>> nomes = new TreeMap<>();
-
-		String path = "E:\\Dev\\Projetos\\sgp-2018\\src\\main\\webapp\\pages\\csv\\Lista-Candidatos-Deputado Distrital-DISTRITO FEDERAL-2018.xls";
-		String str = "";
-
-		try (BufferedReader read = new BufferedReader(new FileReader(path));) {
-			while ((str = read.readLine()) != null) {
-				String[] list = str.split("[;]");
-				String filds = list[0].toString();
-				List<String> distritais = nomes.get(filds);
-				if (distritais == null) {
-					distritais = new ArrayList<>();
-				}
-				distritais.add(filds);
-				nomes.put(filds, distritais);
-
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return nomes;
-
-	}
-
-	*/
-	/*public Map<String, List<String>> listaCandidatosFederal(){
-		Map<String, List<String>> nomes = new TreeMap<>();
-		
-		String path = "E:\\Dev\\Projetos\\sgp-2018\\src\\main\\webapp\\pages\\csv\\Lista-Candidatos-Deputado Federal-DISTRITO FEDERAL-2018.xls";
-		String str = "";
-		
-		try (BufferedReader read  =  new BufferedReader(new FileReader(path))){
-			while((str = read.readLine()) !=null) {
-				String[] list = str.split("[;]");
-				String filds  = list[0].toString();
-				List<String>  federais = nomes.get(filds);
-				if(federais == null) {
-					federais = new ArrayList<>();
-				}
-				
-				federais.add(filds);
-				nomes.put(filds, federais);
-			}
-		}catch (Exception e) {
-			// TODO: handle exception
-		}
-		return nomes;
-	}
-	
-	
-	public Map<String, List<String>> listaCandidatosSenador(){
-		Map<String, List<String>> nomes = new TreeMap<>();
-		
-		String path = "E:\\Dev\\Projetos\\sgp-2018\\src\\main\\webapp\\pages\\csv\\Lista-Candidatos-Senador-DISTRITO FEDERAL-2018.xls";
-		String str = "";
-		
-		try (BufferedReader read  =  new BufferedReader(new FileReader(path))){
-			while((str = read.readLine()) !=null) {
-				String[] list = str.split("[;]");
-				String filds  = list[0].toString();
-				List<String>  senadores = nomes.get(filds);
-				if(senadores == null) {
-					senadores = new ArrayList<>();
-				}
-				
-				senadores.add(filds);
-				nomes.put(filds, senadores);
-			}
-		}catch (Exception e) {
-			// TODO: handle exception
-		}
-		return nomes;
-	}*/
 	
 	public Candidato getCandidato() {
 		return candidato;
